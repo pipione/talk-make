3b489badcbbe2a42f8a83a0936bf1ee590685274
范强林 <46072182+pipione@users.noreply.github.com>
2020-10-20 00:46:37 +0800

    feat: done struct

---
+++ web/front/TODO.md
@@ -0,0 +1,7 @@
+1. 找好看博客的素材
+
+https://www.yangqq.com/
+
+2. 风格不错的网站
+
+https://www.apple.com.cn/
---
+++ web/front/src/pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<!--关于我的介绍-->
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style   scoped>
+
+</style>
